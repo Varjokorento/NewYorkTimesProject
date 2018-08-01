09186de37f2d3a87ea9fee98b39638d4ab5e28f1
Kalle Pusa <kalle.pusa@gmail.com>
2018-08-01 17:09:19 +0300

    First commit

---
+++ routes/books.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+var router = express.Router();
+var parser = require('body-parser');
+var request = require('request');
+var url = "";
+
+
+router.post("/", function(req,res, next) {
+    console.log("hello");
+    var name = req.body.name;
+    var kirjoittaja = req.body.author;
+    console.log(name);
+    console.log(kirjoittaja);
+    request.get({
+        url: "https://api.nytimes.com/svc/books/v3/reviews.json",
+        qs: {
+            'api-key': "bf68c93b35e446589a47004179fd0f97",
+            'author': kirjoittaja
+        },
+    }, function(err, response, body) {
+        body = JSON.parse(body);
+        url =body["results"][0]["url"];
+        var summary = body["results"][0]["summary"];
+        res.render("books", {name: name, kirjoittaja: kirjoittaja, url:url, summary: summary})
+    })
+});
+
+module.exports = router;
